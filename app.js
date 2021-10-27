32995bfa5067c02cede2e0f3223d8fbf52e22e86
Garrett Lu <gbl254@nyu.edu>
2021-10-27 14:43:33 -0400

    milestone 1: project setup

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 
+// node modules
+const path = require("path");
 const express = require("express");
+
+// mongoose models
+const { User, Review, Coaster, Park } = require("./db.js");
+
+// app setup
 const app = express();
 app.set("view engine", "hbs");
+app.use(express.static(path.resolve(__dirname, "public")));
 
-require("db.js");
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000);
